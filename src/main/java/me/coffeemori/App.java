3a77a4c7836336f26e8300c4e42c79cd4d575946
coffeemori92 <coffeemori92@naver.com>
2020-12-16 15:10:11 +0900

    spring 설정

--- src/main/java/me/coffeemori/App.java
+++ src/main/java/me/coffeemori/App.java
@@ -1,9 +1,15 @@
 package me.coffeemori;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootConfiguration
+//@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication // SpringBootConfiguration + ComponentScan + EnableAutoConfiguration
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
